314eeabd8dda9e62ee3cc157d336408f20bbdc3a
Max Bay <cwang167@my.centennialcollege.ca>
2021-02-16 21:44:40 +0800

    Part 5 - UI and React

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -1,21 +1,30 @@
-const initialState = {
-    status: 'All',
-    colors: []
+export const StatusFilters = {
+    All: 'all',
+    Active: 'active',
+    Completed: 'completed',
 }
 
-export default function filtersReducer(state=initialState, action){
+const initialState = {
+    status: StatusFilters.All,
+    colors: [],
+}
+
+export default function filtersReducer(state = initialState, action) {
     switch (action.type) {
-        case 'filters/statusFilterChanged':
+        case 'filters/statusFilterChanged': {
             return {
+                // Again, one less level of nesting to copy
                 ...state,
-                status: action.payload
+                status: action.payload,
             }
-        case 'filters/colorFilterChanged':{
+        }
+        case 'filters/colorFilterChanged': {
             let { color, changeType } = action.payload
+            const { colors } = state
 
             switch (changeType) {
                 case 'added': {
-                    if (state.colors.includes(color)) {
+                    if (colors.includes(color)) {
                         // This color already is set as a filter. Don't change the state.
                         return state
                     }
--- src/index.js
+++ src/index.js
@@ -3,39 +3,42 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import store from './store'
+import {Provider} from 'react-redux'
 
 import './api/server'
 
-console.log('Initial state: ', store.getState())
-
-const unsubscribe = store.subscribe(()=>{
-    console.log('Store after dispatch: ' + store.getState())
-})
-
-console.log('Dispatching action')
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-console.log('Dispatch complete')
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
-
-store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
-store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
-
-store.dispatch({
-    type: 'filters/colorFilterChanged',
-    payload: { color: 'red', changeType: 'selected' }
-})
-
-unsubscribe();
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
+// console.log('Initial state: ', store.getState())
+//
+// const unsubscribe = store.subscribe(()=>{
+//     console.log('Store after dispatch: ' + store.getState())
+// })
+//
+// console.log('Dispatching action')
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
+// console.log('Dispatch complete')
+//
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
+//
+// store.dispatch({ type: 'todos/todoToggled', payload: 0 })
+// store.dispatch({ type: 'todos/todoToggled', payload: 1 })
+//
+// store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
+//
+// store.dispatch({
+//     type: 'filters/colorFilterChanged',
+//     payload: { color: 'red', changeType: 'selected' }
+// })
+//
+// unsubscribe();
+//
+// store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
